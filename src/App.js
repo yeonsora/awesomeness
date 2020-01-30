0f24ad6abfd29084e9fc8c187985e542361155cb
unknown <sryeon@fasoo.com>
2020-01-30 17:32:47 +0900

    style theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import styled, {injectGlobal, keyframes, css} from "styled-components";
+import styled, {injectGlobal, ThemeProvider} from "styled-components";
+import theme from "./theme";
 
 // body css설정을 위해 사용. 따로 component를 안만들어도 된다.
 injectGlobal`
@@ -9,15 +10,6 @@ injectGlobal`
   }
 `;
 
-// 나는 쉬운데, 디자이너들은 어려운거 아님?
-//mixin : css그룹. 내가 그룹화 하고 싶은것. 재사용성이 뛰어남.
-//shared해서 사용하면 됨.
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.88);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
 
 // styled Div Component
 const Container = styled.div`
@@ -26,22 +18,30 @@ const Container = styled.div`
   background-color: pink;
 `;
 
-// styled Input Component
-const Input = styled.input.attrs({
-  required: true // placeholder와 같은 속성을 custom하게 만들기 위해 사용.
-})`
-  border : none;
-  ${awesomeCard}; // css그룹을 적용
+const Card = styled.div`
+  background-color:red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding:25px 15px;
+  background-color: ${props => props.theme.successColor}
 `;
 
 class App extends Component {
   render() {
-    // console.log(Container)
-    return <Container>
-      <Input placeholder="hello" />
-    </Container>;
+    return (
+      // 테마 적용....  Button-> parents에서 뭐 적용하고 그럴 필요가 없음!!!
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Form />
+      </Container>
+    </ThemeProvider>
+    )
   }
 }
+
+const Form = () => (<Card><Button>Hello</Button></Card>)
 
 
 export default App;
